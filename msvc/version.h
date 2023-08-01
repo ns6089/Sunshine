4da3d942f619e645f4335c6cb2151680f2c1322a
ns6089 <61738816+ns6089@users.noreply.github.com>
2023-08-01 17:02:52 +0300

    Add MSVC cmake and vcpkg files

---
+++ msvc/version.h
@@ -0,0 +1,12 @@
+/**
+ * @file src/version.h.in
+ * @brief Version definitions for Sunshine.
+ * @note The final `version.h` is generated from this file during the CMake build.
+ */
+#pragma once
+
+#define PROJECT_NAME "Sunshine"
+#define PROJECT_VER  "0.20.0.msvc"
+#define PROJECT_VER_MAJOR "0"
+#define PROJECT_VER_MINOR "20"
+#define PROJECT_VER_PATCH "0"
